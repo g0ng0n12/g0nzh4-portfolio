ebdf1750725a5e44bbafb8a5650510dc4638880d
Gon Gis <gon.eu.1987@gmail.com>
2020-01-01 15:26:28 -0300

    adding templates

--- webportfolio/settings.py
+++ webportfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'webportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
