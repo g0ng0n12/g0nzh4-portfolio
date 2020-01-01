e2bda6ac33ed6f9c51961029fa264d78cf394ce0
Gon Gis <gon.eu.1987@gmail.com>
2020-01-01 15:33:53 -0300

    updating models

--- app/models.py
+++ app/models.py
@@ -3,19 +3,32 @@ from django.db import models
 
 # Create your models here.
 class Job(models.model):
-    name = models.CharField(max_lenght=50)
+    EMPLOYMENT_TYPE = [("FULL-TIME", "FULL TIME"),
+                       ("PART-TIME", "PART TIME"),
+                       ("SELF-EMPLOYED", "SELF-EMPLOYED"),
+                       ("FREELANCE", "FREELANCE")]
+
+    title = models.CharField(max_lenght=50)
+    company = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    employment_type = models.CharField(choices=EMPLOYMENT_TYPE)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Certification(models.Model):
     name = models.CharField(max_lenght=50)
     image = models.ImageField(upload_to='images/')
+    issuing_organization = models.CharField(max_lenght=50)
     description = models.CharField(max_length=200)
     finish_date = models.DateTimeField()
-    url = models.CharField()
+    url = models.URLField(max_length=200)
+    certification_id = models.CharField(max_lenght=50)
+
+    def __str__(self):
+        return self.name
