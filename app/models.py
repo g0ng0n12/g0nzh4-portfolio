5319f192b4d90176af0a6f94768582bec18a89f1
Gon Gis <gon.eu.1987@gmail.com>
2020-01-01 17:31:51 -0300

    updating model

--- app/models.py
+++ app/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 # Create your models here.
-class Job(models.model):
+class Job(models.Model):
     EMPLOYMENT_TYPE = [("FULL-TIME", "FULL TIME"),
                        ("PART-TIME", "PART TIME"),
                        ("SELF-EMPLOYED", "SELF-EMPLOYED"),
                        ("FREELANCE", "FREELANCE")]
 
-    title = models.CharField(max_lenght=50)
+    title = models.CharField(max_length=50)
     company = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
-    employment_type = models.CharField(choices=EMPLOYMENT_TYPE)
+    employment_type = models.CharField(choices=EMPLOYMENT_TYPE, max_length=70)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
     start_date = models.DateTimeField()
@@ -22,13 +22,13 @@ class Job(models.model):
 
 
 class Certification(models.Model):
-    name = models.CharField(max_lenght=50)
+    name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
-    issuing_organization = models.CharField(max_lenght=50)
+    issuing_organization = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     finish_date = models.DateTimeField()
     url = models.URLField(max_length=200)
-    certification_id = models.CharField(max_lenght=50)
+    certification_id = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
