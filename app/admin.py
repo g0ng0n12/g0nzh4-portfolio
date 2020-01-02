a1fab3b9d2eaafc9af67b7ad8480e71e94cbeca0
Gon Gis <gon.eu.1987@gmail.com>
2020-01-02 16:17:25 -0300

    adding registrationn of the models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Job, Certification, Project
 
-# Register your models here.
+
+admin.site.register(Job)
+admin.site.register(Certification)
+admin.site.register(Project)
