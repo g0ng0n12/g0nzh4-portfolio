8913141e3c9f90ab5037f41f766e7a8e27163fc6
Gon Gis <gon.eu.1987@gmail.com>
2020-01-04 16:34:32 -0300

    added has certification boolean field

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Technology(models.Model):
     name = models.CharField(max_length=50, unique=True)
     yoe = models.IntegerField()
+    has_certification = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
